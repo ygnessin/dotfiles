bfeddc78ba7738a35f27afe69393ee90fd1a695d
Yuval Gnessin <ygnessin@users.noreply.github.com>
2017-01-09 14:35:22 -0800

    Create karabiner-import.sh
    Add settings for Karabiner

---
+++ karabiner-import.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.controlL2commandL_extermvm 1
+/bin/echo -n .
+$cli set remap.pc_application2fn 1
+/bin/echo -n .
+$cli set remap.pc_keyboard_function_standard 1
+/bin/echo -n .
+$cli set repeat.initial_wait 300
+/bin/echo -n .
+$cli set repeat.wait 23
+/bin/echo -n .
+/bin/echo
